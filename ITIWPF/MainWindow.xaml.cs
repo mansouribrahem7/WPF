f25ae432f7852df33a7d032ed4fd18dd1841e14a
Mansour Ibrahim <mansour.ibrahim@esrinea.local>
2023-10-10 13:31:35 +0300

    first commit

---
+++ ITIWPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITIWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        int _count;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(prop));
+        }
+
+        public int Count
+        {
+
+            get
+            {
+                return _count;
+            }
+
+            set
+            {
+                if (_count!=value)
+                {
+                    _count = value;
+                    OnPropertyChanged("Count");
+
+
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void changeCount(object sender, RoutedEventArgs e)
+        {
+            Count++;
+        }
+    }
+}
